837ae14ed3da6acf8a1e27d7e6af59122257aa05
Nikolah <nikolahp13@gmail.com>
2018-08-13 20:22:16 +1200

    Rearrangement of files into groups.

---
+++ MediaLibraryManager/Media.swift
@@ -0,0 +1,76 @@
+//
+//  Media.swift
+//  MediaLibraryManager
+//
+//  Created by Vivian Breda on 13/08/18.
+//  Copyright © 2018 Paul Crane. All rights reserved.
+//
+
+import Foundation
+
+/**
+Creates the metadata for a media file.
+
+*/
+class MetaData: MMMetadata {
+	
+	var keyword: String    //metadata property
+	var value: String      //value of the metadata property
+	
+	
+	/**
+	Designated initialiser
+	
+	Metadata property/value pair is passed in the arguments of the initialiser.
+	
+	- parameter keyword: MetaData's keyword
+	- parameter value: MetaData's value
+	*/
+	init(keyword: String, value: String) {
+		self.keyword = keyword
+		self.value = value
+	}
+	
+	/**
+	String representation of the metadata
+	
+	- returns: String String representation of the metadata.
+	*/
+	var description: String {
+		return "\(self.keyword): "+"\(self.value)"
+	}
+}
+
+/**
+Creates a media file with metadata.
+
+*/
+class File: MMFile {
+	var metadata: [MMMetadata]    //the collection of the file's metadata
+	var filename: String          //the name of the file
+	var path: String              //the path to the file
+	
+	/**
+	Designated initialiser
+	
+	The properties of the file.
+	
+	- parameter metadata: all the metadata of a file.
+	- parameter filename: the name of the file.
+	- parameter path: File's path.
+	*/
+	init(metadata: [MMMetadata], filename: String, path: String) {
+		self.metadata = metadata
+		self.filename = filename
+		self.path = path
+	}
+	
+	/**
+	String representation of the file
+	
+	- returns: String String representation of the file.
+	*/
+	var description: String {
+		return "\(filename)"
+	}
+}
--- MediaLibraryManager/MediaLibrary.swift
+++ MediaLibraryManager/MediaLibrary.swift
@@ -8,69 +8,77 @@
 
 import Foundation
 
-/**
- Creates the metadata for a media file.
- 
- */
-class MetaData: MMMetadata {
-    
-    var keyword: String    //metadata property
-    var value: String      //value of the metadata property
-    
-    
-    /**
-     Designated initialiser
-     
-     Metadata property/value pair is passed in the arguments of the initialiser.
-     
-     - parameter keyword: MetaData's keyword
-     - parameter value: MetaData's value
-     */
-    init(keyword: String, value: String) {
-        self.keyword = keyword
-        self.value = value
-    }
-    
-    /**
-     String representation of the metadata
-     
-     - returns: String String representation of the metadata.
-     */
-    var description: String {
-        return "\(self.keyword): "+"\(self.value)"
-    }
-}
+class MediaLibrary : MMCollection {
+	
+	/**
+	String representation of the Media Library collection
 
-/**
- Creates a media file with metadata.
- 
- */
-class File: MMFile {
-    var metadata: [MMMetadata]    //the collection of the file's metadata
-    var filename: String          //the name of the file
-    var path: String              //the path to the file
-    
-    /**
-     Designated initialiser
-     
-     The properties of the file.
-     
-     - parameter metadata: all the metadata of a file.
-     - parameter filename: the name of the file.
-     - parameter path: File's path.
-     */
-    init(metadata: [MMMetadata], filename: String, path: String) {
-        self.metadata = metadata
-        self.filename = filename
-        self.path = path
-    }
-    
-    /**
-     String representation of the file
-     
-     - returns: String String representation of the file.
-     */
-    var description: String {
-        return "\(filename)"
-    }
+	- returns: String String representation of the library.
+	*/
+	var description: String {
+		// Possibly something about the size of library e.g. num files?
+		return "Library!"
+	}
+	
+	///
+	/// Adds a file's metadata to the media metadata collection.
+	///
+	/// - Parameters:
+	/// - file: The file and associated metadata to add to the collection
+	func add(file: MMFile) {
+		
+	}
+	
+	///
+	/// Adds a specific instance of a metadata to the collection
+	///
+	/// - Parameters:
+	/// - metadata: The item to add to the collection
+	func add(metadata: MMMetadata, file: MMFile)  {
+		
+	}
+	
+	///
+	/// Removes a specific instance of a metadata from the collection
+	///
+	/// - Parameters:
+	/// - metadata: The item to remove from the collection
+	func remove(metadata: MMMetadata)  {
+		
+	}
+	
+	///
+	/// Finds all the files associated with the keyword
+	///
+	/// - Parameters:
+	/// - keyword: The keyword to search for
+	/// - Returns:
+	/// A list of all the metadata associated with the keyword, possibly an
+	/// empty list.
+	func search(term: String) -> [MMFile]  {
+		return []
+	}
+	
+	///
+	/// Returns a list of all the files in the index
+	///
+	/// - Parameters:
+	/// - Returns:
+	/// A list of all the files in the index, possibly an empty list.
+	func all() -> [MMFile]  {
+		return []
+	}
+	
+	///
+	/// Finds all the metadata associated with the keyword of the item
+	///
+	/// - Parameters:
+	/// - item: The item's keyword to search for.
+	/// - Returns:
+	/// A list of all the metadata associated with the item's keyword, possibly
+	/// an empty list.
+	func search(item: MMMetadata) -> [MMFile]  {
+		return []
+	}
+	
 }
