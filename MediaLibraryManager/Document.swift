3e8e9f8efb121a8c38bcc1ec448ba7c2b8aaee34
Nikolah <nikolahp13@gmail.com>
2018-08-21 17:41:13 +1200

    Created Document class to complete inheritance set. References #2

---
+++ MediaLibraryManager/Document.swift
@@ -0,0 +1,14 @@
+//
+//  Document.swift
+//  MediaLibraryManager
+//
+//  Created by Nikolah Pearce on 21/08/18.
+//  Copyright © 2018 Paul Crane. All rights reserved.
+//
+
+import Foundation
+
+class Document : File {
+	
+	
+}
