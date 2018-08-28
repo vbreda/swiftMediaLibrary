aa7aa5f879811b4ba119aff40007870b960e4c66
Vivian Breda <vivianbreda@gmail.com>
2018-08-28 19:46:59 +1200

    Improve FileExporter class to write file to any given location. References issue #5.

--- MediaLibraryManager/FileExporter.swift
+++ MediaLibraryManager/FileExporter.swift
@@ -9,39 +9,50 @@
 import Foundation
 
 class FileExporter: MMFileExport {
-
-	/**
-	 Support exporting the media collection to a file (by name).
-	
-	- parameter filename: somthin.
-	- parameter items:
-	*/
+    
+    /**
+     Support exporting the media collection to a file (by name).
+     
+     - parameter filename: somthin.
+     - parameter items:
+     */
     func write(filename: String, items: [MMFile]) throws {
-		
+        
         var expFiles = [Media]()
         var mdata = [String:String]()
-		
-		// Fill up the Media struct with the files and metadata to save
+        
+        // Fill up the Media struct with the files and metadata to save
         for f in items {
+            mdata = [:]
             for m in f.metadata {
                 mdata.updateValue(m.value, forKey: m.keyword)
             }
             let filetoAdd = Media(fullpath: "\(f.path)/\(f.filename)", type: f.type, metadata: mdata)
             expFiles.append(filetoAdd)
         }
-		
-		// Determine the directory of the users
-        let documentsDirectoryURL = try! FileManager().url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        let fileToWrite = documentsDirectoryURL.appendingPathComponent(filename)
-		
-		// Serialise the data out to file
+        
+        // Determine the directory of the users
+        let fileManager = FileManager.default
+        var fileToWrite: URL
+        
+        if filename.hasPrefix("/") || filename.hasPrefix("~") {
+            let directory = NSString(string: filename).expandingTildeInPath
+            fileToWrite = URL(fileURLWithPath: directory)
+        } else {
+            let working = fileManager.currentDirectoryPath
+            let workingDirectory = URL(fileURLWithPath: working)
+            
+            fileToWrite = workingDirectory.appendingPathComponent(filename)
+        }
+        
+        // Serialise the data out to file
         do {
             let encodedData = try JSONEncoder().encode(expFiles)
             try encodedData.write(to: fileToWrite)
         } catch {
             print("Unable to write", error)
-			print("> Unable to write JSON file...")
-			print("\tCheck your last search and try again.")
+            print("> Unable to write JSON file...")
+            print("\tCheck your last search and try again.")
         }
     }
 }
